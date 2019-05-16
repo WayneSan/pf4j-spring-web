c5d0fefe428b5999d535a0be9b34f2a04859827b
WayneSan <waynesan@zerozone.tw>
2019-05-16 17:13:33 +0800

    add scaffold for spring-boot-starter and pf4j-spring

---
+++ pf4j-spring-web/src/main/java/com/example/spring/web/Pf4jSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pf4jSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pf4jSpringWebApplication.class, args);
+    }
+
+}
